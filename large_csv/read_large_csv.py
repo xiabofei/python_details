454ce7d7d28b1587a0607f630b806a2979ae6686
xiabofei <xbf9xbf@qq.com>
2017-02-06 17:37:51 +0800

    read large csv file

---
+++ large_csv/read_large_csv.py
@@ -0,0 +1,18 @@
+#encoding=utf8
+
+"""
+how use pandas to load large csv file
+1. not that large needs distributed network
+2. too large for once loading in memory but can fit hard-disk
+"""
+import pandas as pd
+
+path = './test_data.csv'
+
+# print pd.read_csv(path, nrows=1)
+
+i = 0
+for df in pd.read_csv(path, chunksize=100000, iterator=True):
+    # df.to_pickle('./df_output_'+str(i)+'.pkl')
+    df.to_csv('./df_output_'+str(i)+'.csv')
+    i += 1
