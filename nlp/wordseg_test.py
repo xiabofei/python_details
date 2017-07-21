6284497381a68c1ac8df89fc1ea751ea1b4d4dbf
xiabofei <xbf9xbf@qq.com>
2017-07-21 10:41:08 +0800

    add suggest freq

--- nlp/wordseg_test.py
+++ nlp/wordseg_test.py
@@ -3,6 +3,7 @@
 import jieba
 import jieba.posseg as poseg
 import pandas as pd
+from collections import OrderedDict
 
 from ipdb import set_trace as st
 
@@ -16,6 +17,8 @@ columns = ['ExamDescExResult', 'ResultDescExResult']
 
 usr_dict_path = './data/input/mri_dict.dat'
 
+usr_suggest_path = './data/input/mri_suggest.dat'
+
 
 def _add_usr_dict(path):
     with open(path, 'r') as f:
@@ -23,6 +26,11 @@ def _add_usr_dict(path):
             items = l.split(',')
             jieba.add_word(items[0].rstrip(), items[1].rstrip(), items[2].rstrip())
 
+def _suggest_usr_dict(path):
+    with open(path, 'r') as f:
+        for l in f.xreadlines():
+            word1, word2 = l.split(',')[0].rstrip(), l.split(',')[1].rstrip()
+            jieba.suggest_freq((word1, word2), True)
 
 def _replace_punctuation(content):
     _chinese_english = [
@@ -66,11 +74,17 @@ def _cut(content):
 
 
 def _find_topic_range(content, topics):
+    _legal_punctuation = (':',)
+    _pos_punctuation = (',', ';', '.',)
     ret = []
     for topic in topics:
         pos = content.find(topic)
         if pos >= 0:
-            ret.append([pos, topic])
+            if content[pos + len(topic):pos + len(topic) + 1] not in _legal_punctuation:
+                _nearest_right_comma_pos = max([content[:pos].rfind(p) for p in _pos_punctuation]) + 1
+                ret.append([_nearest_right_comma_pos, topic])
+            else:
+                ret.append([pos, topic])
     ret = sorted(ret, key=lambda x: x[0])
     for i in range(len(ret) - 1):
         ret[i][0] = (ret[i][0], ret[i + 1][0])
@@ -85,16 +99,17 @@ def pre_process(content, process_flow):
     return ret
 
 
-_mri_topic = ('宫体', '宫颈', '阴道及外阴', '双侧附件', '淋巴结', '其他', '盆腔积液', '骨盆骨质')
-
-_mri_type_one_words = ('T1', 'T2')
-
-_deny_word = (u'不伴', u'未见', u'未',)
-
-_ensure_word = (u'可见', u'伴')
+# _mri_topic = ('宫体', '宫颈', '阴道及外阴', '双侧附件', '淋巴结', '其他', '盆腔积液', '骨盆骨质')
 
 
-def extract_type_one_words(topic_content, words):
+
+_deny_word = (u'不伴', u'未见', u'未', u'无',)
+
+_ensure_word = (u'可见', u'伴',)
+
+
+def extract_type_one_words(topic_content):
+    words = ('T1', 'T2')
     ret = []
     _des_pattern = ('n+v', 'v+n', 'u+n', 'd+l', 'a+n', 'n+ns', 'n+a')
     _degree_pos = ('u', 'a', 'm')
@@ -143,10 +158,8 @@ def extract_type_one_words(topic_content, words):
     return ret
 
 
-_mri_type_two_words = (u'结合带',)
-
-
-def extract_type_two_words(topic_content, words):
+def extract_type_two_words(topic_content):
+    words = (u'结合带',)
     ret = []
     for index, tc in enumerate(topic_content):
         if tc[0] in words:
@@ -160,9 +173,47 @@ def extract_type_two_words(topic_content, words):
     return ret
 
 
+def extract_type_three_words(topic_content):
+    words = (u'盆腔积液',)
+    ret = []
+    for index, tc in enumerate(topic_content):
+        if tc[0] in words:
+            has_or_not = '有'
+            # nearest after
+            for i in range(index + 1, len(topic_content)):
+                if topic_content[i][1] == 'x':
+                    break
+                if topic_content[i][0] in _deny_word:
+                    has_or_not = '无'
+                    break
+            # nearest before
+            for i in range(index, -1, -1):
+                if topic_content[i][1] == 'x':
+                    break
+                if topic_content[i][0] in _deny_word:
+                    has_or_not = '无'
+                    break
+            ret.append('指标:' + tc[0].encode('utf-8') + ',' + '有无:' + has_or_not)
+            break
+    return ret
+
+
+_mri_topic_extract_methods = OrderedDict([
+    ('宫体', [extract_type_one_words, extract_type_two_words]),
+    ('宫颈', [extract_type_one_words, extract_type_two_words]),
+    ('阴道及外阴', [extract_type_one_words, extract_type_two_words]),
+    ('双侧附件', [extract_type_one_words, extract_type_two_words]),
+    ('淋巴结', [extract_type_one_words, extract_type_two_words]),
+    ('其它', [extract_type_one_words, extract_type_two_words]),
+    ('盆腔积液', [extract_type_one_words, extract_type_three_words]),
+    ('骨盆骨质', []),
+])
+
 with open(input_path, 'r') as f_input:
     df = pd.read_csv(f_input)
+    # st(context=21)
     _add_usr_dict(usr_dict_path)
+    _suggest_usr_dict(usr_suggest_path)
     with open(output_path + 'mri_describe_result', 'w') as f_output:
         for i in df.index:
             # show origin mri report text
@@ -172,19 +223,19 @@ with open(input_path, 'r') as f_input:
                                  [_replace_punctuation, _negative_positive])
             f_output.write('[报告原文-全]:' + CLRF)
             f_output.write(content + CLRF)
-            # f_output.write('[报告解读]:' + CLRF)
-            # f_output.write(result + CLRF)
+            f_output.write(CLRF + '[报告解读]:' + CLRF)
+            f_output.write(result + CLRF)
             # show word seg result
-            # f_output.write('[词性标注结果]:' + CLRF)
-            topic_range = _find_topic_range(content, _mri_topic)
+            topic_range = _find_topic_range(content, _mri_topic_extract_methods.keys())
             for item in topic_range:
                 begin, end, topic = item[0][0], item[0][1], item[1]
                 f_output.write(CLRF + "[报告原文-%s]" % (topic) + CLRF)
                 f_output.write(content[begin:end] + CLRF)
                 topic_content = [(k, v) for k, v in poseg.cut(content[begin:end], HMM=False)]
                 f_output.write('[信号提取结果-%s]:' % (topic) + CLRF)
-                f_output.write(CLRF.join(extract_type_one_words(topic_content, _mri_type_one_words)) + CLRF)
-                # f_output.write(CLRF.join(extract_type_two_words(topic_content, _mri_type_two_words)) + CLRF)
-                # f_output.write('\t' + ''.join(
-                #     [i[0].encode('utf8') + '/' + i[1].encode('utf8') + ' ' for i in topic_content]) + CLRF)
-            f_output.write('='*80+CLRF)
+                for method in _mri_topic_extract_methods[topic]:
+                    f_output.write(CLRF.join(method(topic_content)) + CLRF)
+                f_output.write("[分词结果-%s]" % (topic) + CLRF)
+                f_output.write('\t' + ''.join(
+                    [i[0].encode('utf8') + '/' + i[1].encode('utf8') + ' ' for i in topic_content]) + CLRF)
+            f_output.write('=' * 80 + CLRF)
