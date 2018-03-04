bd4fba145634fdba286984c584f70e09e66095ec
xiabofei <xbf9xbf@qq.com>
2018-03-04 20:35:59 +0800

    no stopwords maxpool_gru and skip_gru and maxpool cnn

--- kaggle/toxic-comment/script/model_maxpool_cnn.py
+++ kaggle/toxic-comment/script/model_maxpool_cnn.py
@@ -8,22 +8,20 @@ import pandas as pd
 import numpy as np
 from numpy import asarray
 
-from keras.layers.advanced_activations import LeakyReLU
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.layers import Dense
 from keras.layers import Embedding
-from keras.layers import Conv1D, Conv2D, MaxPool2D, Flatten
-from keras.layers import AvgPool2D
-from keras.layers import Concatenate, Add
+from keras.layers import Conv1D
+from keras.layers import GlobalMaxPooling1D
 from keras.layers import Input
 from keras.layers import Dropout
-from keras.layers import Reshape
 from keras.layers import BatchNormalization
 from keras.layers import SpatialDropout1D
+from keras.layers import concatenate
 
 from keras.models import Model
-from keras.optimizers import RMSprop, Nadam, Adam
+from keras.optimizers import Nadam
 
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 
@@ -31,7 +29,6 @@ from data_split import label_candidates
 from comm_preprocessing import data_comm_preprocessed_dir
 from comm_preprocessing import COMMENT_COL, ID_COL
 from comm_preprocessing import toxicIndicator_transformers
-# from comm_preprocessing_lighter import toxicIndicator_transformers
 
 from roc_auc_metric import RocAucMetricCallback
 from roc_auc_metric import VAL_AUC
@@ -49,6 +46,7 @@ import gc
 
 # mode = 'try'
 mode = 'other'
+
 
 def read_data_in_fold(k):
     df_trn = pd.read_csv(data_comm_preprocessed_dir + '{0}_train.csv'.format(k))
@@ -75,11 +73,8 @@ def read_train_data():
 def get_fitted_tokenizer(df_train, df_test):
     comments_train = df_train[COMMENT_COL].values.tolist()
     comments_test = df_test[COMMENT_COL].values.tolist()
-    tokenizer = Tokenizer()
     # remain '!' and '?'
     tokenizer = Tokenizer(filters='"#$%&()*+,-./:;<=>@[\\]^_`{|}~\t\n')
-    # tokenizer = Tokenizer()
-    # tokenizer.num_words = MAX_NUM_WORDS
     tokenizer.fit_on_texts(comments_train + comments_test)
     return tokenizer
 
@@ -124,21 +119,11 @@ def get_embedding_lookup_table(word_index, glove_path, fasttext_path, embedding_
         return ret
 
     nb_words = min(MAX_NUM_WORDS, len(word_index))
+    if mode == 'try':
+        return np.zeros((nb_words, embedding_dim)), np.zeros((nb_words, embedding_dim))
+
     print('! index : {0}'.format(word_index['!']))
     print('? index : {0}'.format(word_index['?']))
-    # get fasttext word vector
-    fasttext_embedding_lookup_table = np.zeros((nb_words, embedding_dim))
-    fasttext_embedding_index = _get_fasttext_embedding_index(fasttext_path)
-    for word, index in word_index.items():
-        if index >= MAX_NUM_WORDS:
-            continue
-        vector = fasttext_embedding_index.get(word)
-        if vector is not None:
-            fasttext_embedding_lookup_table[index] = vector
-    print('fasttext null word embeddings : {0}'.format(
-        np.sum(np.sum(fasttext_embedding_lookup_table, axis=1) == 0)))
-    del fasttext_embedding_index
-    gc.collect()
 
     # get glove word vector
     glove_embedding_index = _get_glove_embedding_index(glove_path)
@@ -153,12 +138,26 @@ def get_embedding_lookup_table(word_index, glove_path, fasttext_path, embedding_
         np.sum(np.sum(glove_embedding_lookup_table, axis=1) == 0)))
     del glove_embedding_index
     gc.collect()
+
+    # get fasttext word vector
+    fasttext_embedding_index = _get_fasttext_embedding_index(fasttext_path)
+    fasttext_embedding_lookup_table = np.zeros((nb_words, embedding_dim))
+    for word, index in word_index.items():
+        if index >= MAX_NUM_WORDS:
+            continue
+        vector = fasttext_embedding_index.get(word)
+        if vector is not None:
+            fasttext_embedding_lookup_table[index] = vector
+    print('fasttext null word embeddings : {0}'.format(
+        np.sum(np.sum(fasttext_embedding_lookup_table, axis=1) == 0)))
+    del fasttext_embedding_index
+    gc.collect()
+
     return glove_embedding_lookup_table, fasttext_embedding_lookup_table
 
 
-def get_model(glove_embedding_lookup_table, fasttext_embedding_lookup_table ,dropout, spatialDropout):
+def get_model(glove_embedding_lookup_table, fasttext_embedding_lookup_table, dropout, spatialDropout):
     print('dropout : {0}'.format(dropout))
-    print('spatial dropout : {0}'.format(spatialDropout))
     input_layer = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
 
     ## Chanel 1 : glove embedding
@@ -169,18 +168,14 @@ def get_model(glove_embedding_lookup_table, fasttext_embedding_lookup_table ,dro
         trainable=False
     )(input_layer)
     glove_embedding_layer = SpatialDropout1D(spatialDropout)(glove_embedding_layer)
-    embedding_dim = glove_embedding_lookup_table.shape[1]
-    filter_sizes = [2,3,4]
+    kernel_sizes = [2, 3, 4]
     num_filters = 128
-    reshape = Reshape((MAX_SEQUENCE_LENGTH, embedding_dim, 1))(glove_embedding_layer)
     glove_multi_filters = []
-    for filter_size in filter_sizes:
-        conv = Conv2D(num_filters, kernel_size=(filter_size, embedding_dim), padding='valid',
-                    kernel_initializer='normal', activation='relu')(reshape)
-        bn = BatchNormalization()(conv)
-        maxpool = MaxPool2D(
-            pool_size=(MAX_SEQUENCE_LENGTH - filter_size + 1, 1), strides=(1, 1), padding='valid')(bn)
-        glove_multi_filters.append(maxpool)
+    for kernel_size in kernel_sizes:
+        conv1d = Conv1D(filters=num_filters, kernel_size=kernel_size, activation='relu')(glove_embedding_layer)
+        bn = BatchNormalization()(conv1d)
+        max_pool = GlobalMaxPooling1D()(bn)
+        glove_multi_filters.append(max_pool)
 
     ## Chanel 2 : fasttext embedding
     fasttext_embedding_layer = Embedding(
@@ -190,21 +185,14 @@ def get_model(glove_embedding_lookup_table, fasttext_embedding_lookup_table ,dro
         trainable=False
     )(input_layer)
     fasttext_embedding_layer = SpatialDropout1D(spatialDropout)(fasttext_embedding_layer)
-    embedding_dim = fasttext_embedding_lookup_table.shape[1]
-    filter_sizes = [2,3,4]
+    kernel_sizes = [2, 3, 4]
     num_filters = 128
-    reshape = Reshape((MAX_SEQUENCE_LENGTH, embedding_dim, 1))(fasttext_embedding_layer)
     fasttext_multi_filters = []
-    for filter_size in filter_sizes:
-        conv = Conv2D(num_filters, kernel_size=(filter_size, embedding_dim), padding='valid',
-                      kernel_initializer='normal', activation='tanh')(reshape)
-        maxpool = MaxPool2D(
-            pool_size=(MAX_SEQUENCE_LENGTH - filter_size + 1, 1), strides=(1, 1), padding='valid')(conv)
-        fasttext_multi_filters.append(maxpool)
-
-    layer = Concatenate(axis=1)(fasttext_multi_filters+glove_multi_filters)
-    layer = BatchNormalization()(layer)
-    layer = Flatten()(layer)
+    for kernel_size in kernel_sizes:
+        conv1d = Conv1D(filters=num_filters, kernel_size=kernel_size, activation='tanh')(fasttext_embedding_layer)
+        max_pool = GlobalMaxPooling1D()(conv1d)
+        fasttext_multi_filters.append(max_pool)
+    layer = concatenate(glove_multi_filters + fasttext_multi_filters)
     layer = Dropout(dropout)(layer)
     output_layer = Dense(6, activation='sigmoid')(layer)
     model = Model(inputs=input_layer, outputs=output_layer)
@@ -224,11 +212,10 @@ def run_one_fold(fold):
     all_words = set(word_index.keys())
     for toxic, transformers in toxicIndicator_transformers.items():
         for transformer in transformers:
-            if transformer==toxic:
+            if transformer == toxic:
                 continue
             if transformer in all_words:
                 transformers_count += tokenizer.word_counts[transformer]
-                # print(transformer)
     print('toxic transformer count : {0}'.format(transformers_count))
     print('unique token : {0}'.format(len(word_index)))
 
@@ -275,7 +262,9 @@ def run_one_fold(fold):
         # callbacks
         val_auc = RocAucMetricCallback()
         es = EarlyStopping(monitor=VAL_AUC, mode='max', patience=5)
-        bst_model_path = '../data/output/model/{0}fold_{1}run_glove_cnn.h5'.format(fold, run)
+        bst_model_path = \
+            '../data/output/model/{0}fold_{1}run_{2}dp_{3}sdp_maxpool_cnn.h5'.format(
+                fold, run, FLAGS.dp, FLAGS.sdp)
         mc = ModelCheckpoint(bst_model_path, save_best_only=True, save_weights_only=True)
         rp = ReduceLROnPlateau(
             monitor=VAL_AUC, mode='max',
@@ -283,7 +272,6 @@ def run_one_fold(fold):
             factor=np.sqrt(0.1),
             verbose=1
         )
-
         # train
         hist = model.fit(
             x=X_trn, y=y_trn,
@@ -311,19 +299,22 @@ def run_one_fold(fold):
     df_preds_test[ID_COL] = id_test
     for idx, label in enumerate(label_candidates):
         df_preds_test[label] = preds_test[idx]
-    df_preds_test.to_csv('../data/output/preds/glove_cnn/{0}/{1}fold_test.csv'.format(FLAGS.dp, fold), index=False)
+    df_preds_test.to_csv(
+        '../data/output/preds/maxpool_cnn/{0}/{1}/{2}fold_test.csv'.format(FLAGS.dp, FLAGS.sdp, fold), index=False)
 
     preds_valid = preds_valid.T
     df_preds_val = pd.DataFrame()
     df_preds_val[ID_COL] = id_val
     for idx, label in enumerate(label_candidates):
         df_preds_val[label] = preds_valid[idx]
-    df_preds_val.to_csv('../data/output/preds/glove_cnn/{0}/{1}fold_valid.csv'.format(FLAGS.dp, fold), index=False)
+    df_preds_val.to_csv(
+        '../data/output/preds/maxpool_cnn/{0}/{1}/{2}fold_valid.csv'.format(FLAGS.dp, FLAGS.sdp, fold), index=False)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--fold', type=str, default='0', help='train on which fold')
     parser.add_argument('--dp', type=str, default='0.625', help='dropout')
-    parser.add_argument('--sdp', type=str, default='0.2', help='dropout')
+    parser.add_argument('--sdp', type=str, default='0.2', help='spatial dropout')
     FLAGS, _ = parser.parse_known_args()
     run_one_fold(FLAGS.fold)
